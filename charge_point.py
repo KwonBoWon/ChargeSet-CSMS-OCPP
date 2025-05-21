d4673124eea09dff97a9aac49d07f0c17fa35900
KwonBoWon <bogod1207@naver.com>
2025-05-21 15:40:36 +0900

    15:40

--- charge_point.py
+++ charge_point.py
@@ -1,6 +1,11 @@
 import asyncio
 import datetime
 import websockets
+import asyncio
+import serial_asyncio
+import serial.tools.list_ports
+import json
+import platform
 
 from ocpp.v201 import ChargePoint as CP
 from ocpp.v201 import call, call_result
@@ -16,6 +21,8 @@ from ocpp.v201.enums import (
     GenericStatusEnumType,
     Iso15118EVCertificateStatusEnumType
 )
+
+candidates = [] # 현재 usb 포트
 
 # TODO 함수 전체적으로 인자값들 조정
 class ChargePoint201(CP):
@@ -109,8 +116,8 @@ async def run_cp(cp):
     except websockets.exceptions.ConnectionClosedOK:
         print("연결이 정상적으로 종료되었습니다.")
 
-async def authorize_transaction_manager(cp):
-    authorzie_response = await cp.send_authorize("token-1234") # TODO 토큰값 넘기기
+async def authorize_transaction_manager(cp, id_token: str = "token-1234", transport=None):
+    authorzie_response = await cp.send_authorize(id_token) # TODO 토큰값 넘기기
     print("authorize_response:") # chargingSchedules 받음
     print(authorzie_response)
     _charging_schedules = authorzie_response.custom_data["charging_schedules"]
@@ -120,6 +127,14 @@ async def authorize_transaction_manager(cp):
     _connector_id = authorzie_response.custom_data['connector_id']
     _start_time = authorzie_response.custom_data['start_time']
     _end_time = authorzie_response.custom_data['end_time']
+
+    # 스케쥴 전송
+    response = {
+        "chargingSchedules": _charging_schedules
+    }
+    json_str = json.dumps(response) + "\n"
+    transport.write(json_str.encode())
+
 
     await cp.start_transaction(
         _user_id, _id_token, _evse_id, _connector_id, _charging_schedules
@@ -133,6 +148,76 @@ async def authorize_transaction_manager(cp):
     await cp.stop_transaction() # TODO트랜잭션 ended로 업데이트
     await asyncio.sleep(1)
 
+"""
+        # TODO 여기에서 무한루프로 플러그앤 차지 기다리면 됨
+        await authorize_transaction_manager(cp)
+        # TODO 받아야 하는 데이터 evseId, connectorId, userId(token),
+"""
+
+
+class ESP32Protocol(asyncio.Protocol):
+    def __init__(self, port, cp):
+        self.buffer = ""
+        self.port = port
+        self.cp = cp
+
+    def connection_made(self, transport):
+        self.transport = transport
+        print(f"ESP{self.port.device} 연결됨")
+
+    def data_received(self, data):
+        self.buffer += data.decode()
+        while '\n' in self.buffer:
+            line, self.buffer = self.buffer.split('\n', 1)
+            line = line.strip()
+            print(f"수신된 ID Token: {line}")
+            asyncio.create_task(authorize_transaction_manager(self.cp, line, self.transport))
+    """
+    print(f"처리 중인 ID Token: {id_token}")
+        response = {
+            "chargingSchedules": [
+                {"startPeriod": 0, "limit": 4000, "useESS": True},
+                {"startPeriod": 1800, "limit": 3000, "useESS": False}
+            ]
+        }
+        json_str = json.dumps(response) + "\n"
+        self.transport.write(json_str.encode())
+    """
+
+    def connection_lost(self, exc):
+        print(f"ESP{self.port.device} 연결 끊김")
+        candidates.remove(self.port.device)
+
+async def find_esp32_port(cp):
+    system = platform.system()
+
+    print(f"ESP32 포트를 기다리는중 (OS:{system})")
+    while True:
+        ports = serial.tools.list_ports.comports()
+        for port in ports:
+            desc = port.description.lower()
+            dev = port.device.lower()
+
+            if system == "Darwin":  # macOS
+                if "usbserial" in dev or "usbmodem" in dev or "cp210" in desc or "ch340" in desc:
+                    if port.device not in candidates:
+                        candidates.append(port.device)
+                        print(port.device)
+                        asyncio.create_task(connect_esp32(port,cp))
+            elif system == "Linux":
+                if "ttyusb" in dev or "ttyacm" in dev:
+                    if port.device not in candidates:
+                        candidates.append(port.device)
+                        print(port.device)
+                        asyncio.create_task(connect_esp32(port, cp))
+        await asyncio.sleep(0.5)
+
+async def connect_esp32(port, cp):
+    loop = asyncio.get_running_loop()
+    await serial_asyncio.create_serial_connection(
+        loop, lambda: ESP32Protocol(port, cp), port.device, baudrate=115200
+    )
+
 async def charge_point_manager(uri, cp_name):
     async with websockets.connect(uri, subprotocols=["ocpp2.0.1"]) as ws:
         cp = ChargePoint201(cp_name, ws)
@@ -141,28 +226,7 @@ async def charge_point_manager(uri, cp_name):
         await cp.send_boot_notification()
         await asyncio.sleep(1)
         print(f"ChargeStation {cp_name} is ready to charge.")
-
-        # TODO 여기에서 무한루프로 플러그앤 차지 기다리면 됨
-        await authorize_transaction_manager(cp)
-        # TODO 받아야 하는 데이터 evseId, connectorId, userId(token),
-"""
-        while True:
-            command = await asyncio.to_thread(input, f"Enter command for {cp_name} (start/stop/exit): ")
-            command = command.strip()
-            if command == "start":
-                print(f"Starting transaction for {cp_name}...")
-                await cp.start_transaction()
-            elif command == "stop":
-                print(f"Stopping transaction for {cp_name}...")
-                await cp.stop_transaction()
-            elif command == "exit":
-                print(f"Exiting {cp_name} management...")
-                break
-            else:
-                print("Unknown command. Please use 'start', 'stop', or 'exit'.")
-"""
-
-
+        await find_esp32_port(cp)
 
 async def main(*args, **kwargs):
     uri = "ws://localhost:9000/"
@@ -170,6 +234,7 @@ async def main(*args, **kwargs):
     await asyncio.gather(
         charge_point_manager(uri + "ST_001", "ST_001"),
         #charge_point_manager(uri + "ST_002", "ST_002"),
+
     )
 
 
--- csms.py
+++ csms.py
@@ -25,7 +25,7 @@ import platform
 __version__ = "0.1.0"
 
 reject_auth = False
-candidates = [] # 현재 usb 포트
+
 
 async def process_request(connection, request):
     logging.info(f'request:\n{request}')
@@ -75,56 +75,7 @@ async def on_connect(websocket, path):
             # 연결 종료될때
             await cp.close_connection()
 
-class ESP32Protocol(asyncio.Protocol):
-    def __init__(self, port):
-        self.buffer = ""
-        self.port = port
 
-    def connection_made(self, transport):
-        self.transport = transport
-        print(f"ESP{self.port.device} 연결됨")
-
-    def data_received(self, data):
-        self.buffer += data.decode()
-        while '\n' in self.buffer:
-            line, self.buffer = self.buffer.split('\n', 1)
-            line = line.strip()
-            print(f"수신된 ID Token: {line}")
-            #self.handle_id_token(line)
-
-    def connection_lost(self, exc):
-        print(f"ESP{self.port.device} 연결 끊김")
-        candidates.remove(self.port.device)
-
-async def find_esp32_port():
-    system = platform.system()
-
-    print(f"ESP32 포트를 기다리는중 (OS:{system})")
-    while True:
-        ports = serial.tools.list_ports.comports()
-        for port in ports:
-            desc = port.description.lower()
-            dev = port.device.lower()
-
-            if system == "Darwin":  # macOS
-                if "usbserial" in dev or "usbmodem" in dev or "cp210" in desc or "ch340" in desc:
-                    if port.device not in candidates:
-                        candidates.append(port.device)
-                        print(port.device)
-                        asyncio.create_task(connect_esp32(port))
-            elif system == "Linux":
-                if "ttyusb" in dev or "ttyacm" in dev:
-                    if port.device not in candidates:
-                        candidates.append(port.device)
-                        print(port.device)
-                        asyncio.create_task(connect_esp32(port))
-        await asyncio.sleep(0.5)
-
-async def connect_esp32(port):
-    loop = asyncio.get_running_loop()
-    await serial_asyncio.create_serial_connection(
-        loop, lambda: ESP32Protocol(port), port.device, baudrate=115200
-    )
 
 
 async def main():
@@ -148,13 +99,13 @@ async def main():
     global reject_auth
     reject_auth = args.reject_auth
 
+
     server = await websockets.serve(
         on_connect, host, port, subprotocols=["ocpp2.0.1"], process_request=process_request
     )
 
     logging.info("OCPP CSMS Started listening to new connections...")
     await server.wait_closed()
-
 
 if __name__ == "__main__":
     try:
